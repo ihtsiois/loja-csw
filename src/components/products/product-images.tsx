1959966c40edbe9bde4a89e5ead8869921426c3f
Achilles Gusmão <achilles@dazzledesign.com.br>
2025-05-07 17:44:35 -0300

    feat: add multiple images support

--- src/components/products/product-images.tsx
+++ src/components/products/product-images.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import { Product } from '@/data/products';
+import { useState } from 'react';
 
 const ProductImages = ({ product }: { product: Product }) => {
+    const [current, setCurrent] = useState(0);
+
     return (
-        <div>
-            <img src={product.images[0]} alt={product.name} className="w-full" />
+        <div className="flex flex-col space-y-4">
+            <img src={product.images[current]} alt={product.name} className="w-full" />
+            <div className="flex gap-2 flex-wrap">
+                {product.images.map((image, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setCurrent(index)}
+                        className="size-18 aspect-square border rounded overflow-hidden cursor-pointer"
+                    >
+                        <img src={image} className="hover:brightness-90 transition-all" />
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
